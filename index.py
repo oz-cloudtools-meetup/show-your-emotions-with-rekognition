f5f10e356757302a7f41347849aed1ab00df1b10
Harris Hall <harris.hall@nab.com.au>
2019-09-03 10:59:47 +1000

    backup

--- index.py
+++ index.py
@@ -8,67 +8,69 @@ IMAGE = "<YOUR_IMAGE>"
 COLLECTION = "<YOUR_COLLECTION>"
 PROCESSED_IMAGE = "<YOUR_PROCESSED_IMAGE>"
 
-# Create an s3 client 's3' and a rekognition client 'reko'
-s3 = boto3.resource('s3') 
-reko = boto3.client('rekognition') 
+if __name__=='__main__':
 
-# Index the face and receive response from rekognition service
-response = reko.index_faces(
-    CollectionId = COLLECTION,
-    Image = {
-        'S3Object': {
-            'Bucket': BUCKET,
-            'Name': IMAGE,
-        }
-    },
-    DetectionAttributes = ['ALL'],
-    MaxFaces = 1,
-    QualityFilter = 'AUTO'
-)
-# print(response)
+    # Create an s3 client 's3' and a rekognition client 'reko'
+    s3 = boto3.resource('s3') 
+    reko = boto3.client('rekognition') 
 
-# Extract face metadata from the response
-info = response['FaceRecords'][0]['FaceDetail']
-smileConfidence = info['Smile']['Confidence']
-smile = info['Smile']['Value']
-gender = info['Gender']['Value']
-agelow = info['AgeRange']['Low']
-agehigh = info['AgeRange']['High']
-faceId = response['FaceRecords'][0]['Face']['FaceId']
+    # Index the face and receive response from rekognition service
+    response = reko.index_faces(
+        CollectionId = COLLECTION,
+        Image = {
+            'S3Object': {
+                'Bucket': BUCKET,
+                'Name': IMAGE,
+            }
+        },
+        DetectionAttributes = ['ALL'],
+        MaxFaces = 1,
+        QualityFilter = 'AUTO'
+    )
+    # print(response)
 
-# Determine emotion by picking the one with highest confidence
-emotions = info['Emotions']
-emotionType = []
-emotionConf = []
-count = 0
-for allEmotions in emotions:
-    emotionType.insert(count, allEmotions['Type'])
-    emotionConf.insert(count, allEmotions['Confidence'])
-    count += 1
-max_value = max(emotionConf)
-max_index = emotionConf.index(max_value)
-emotion = emotionType[max_index]
+    # Extract face metadata from the response
+    info = response['FaceRecords'][0]['FaceDetail']
+    smileConfidence = info['Smile']['Confidence']
+    smile = info['Smile']['Value']
+    gender = info['Gender']['Value']
+    agelow = info['AgeRange']['Low']
+    agehigh = info['AgeRange']['High']
+    faceId = response['FaceRecords'][0]['Face']['FaceId']
 
-print("Information about face ID " + str(faceId) + ":")
-print("Smile: " + str(smile) + "\nThe confidence is: " + str(smileConfidence))
-print("Gender: " + str(gender))
-print("Age between: " + str(agelow) + " to " + str(agehigh))
-print("Emotion: " + str(emotion))
+    # Determine emotion by picking the one with highest confidence
+    emotions = info['Emotions']
+    emotionType = []
+    emotionConf = []
+    count = 0
+    for allEmotions in emotions:
+        emotionType.insert(count, allEmotions['Type'])
+        emotionConf.insert(count, allEmotions['Confidence'])
+        count += 1
+    max_value = max(emotionConf)
+    max_index = emotionConf.index(max_value)
+    emotion = emotionType[max_index]
 
-# Get the image W x H 
-img = cv2.imread(IMAGE,1)
-imgHeight, imgWidth, channels= img.shape
+    print("Information about face ID " + str(faceId) + ":")
+    print("Smile: " + str(smile) + "\nThe confidence is: " + str(smileConfidence))
+    print("Gender: " + str(gender))
+    print("Age between: " + str(agelow) + " to " + str(agehigh))
+    print("Emotion: " + str(emotion))
 
-# Get bounding box values and turn it into drawing coordinates
-box = info['BoundingBox']
-left = int(imgWidth * box['Left'])
-top = int(imgHeight * box['Top'])
-width = int(imgWidth * box['Width']) + left
-height = int(imgHeight * box['Height']) + top
+    # Get the image W x H 
+    img = cv2.imread(IMAGE,1)
+    imgHeight, imgWidth, channels= img.shape
 
-# Draw on the image
-cv2.rectangle(img, (left, top), (width, height), (0, 255, 0), 2)
+    # Get bounding box values and turn it into drawing coordinates
+    box = info['BoundingBox']
+    left = int(imgWidth * box['Left'])
+    top = int(imgHeight * box['Top'])
+    width = int(imgWidth * box['Width']) + left
+    height = int(imgHeight * box['Height']) + top
 
-# Save this image and send it to s3
-cv2.imwrite(PROCESSED_IMAGE, img)
-s3.meta.client.upload_file(PROCESSED_IMAGE, BUCKET, PROCESSED_IMAGE)
+    # Draw on the image
+    cv2.rectangle(img, (left, top), (width, height), (0, 255, 0), 2)
+
+    # Save this image and send it to s3
+    cv2.imwrite(PROCESSED_IMAGE, img)
+    s3.meta.client.upload_file(PROCESSED_IMAGE, BUCKET, PROCESSED_IMAGE)
