1cd258074d3e087f41ecadbd64832b99ce6d12dd
Simon <simonmackinnon@users.noreply.github.com>
2019-09-11 12:07:51 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ sudo yum install npm -y
 npm install -g c9
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
-pip3 install boto3 --user
-pip3 install opencv-python --user
+sudo pip install -r requirements.txt
 
 echo "==========> Dependencies installed successfully."
